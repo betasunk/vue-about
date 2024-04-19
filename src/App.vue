339c1dd373ddffa202783586007ccd88e0c85443
betasunk <979991000@qq.com>
2024-04-19 16:49:49 +0800

    feat: 添加路由

--- src/App.vue
+++ src/App.vue
@@ -1,46 +1,32 @@
 <template>
   <div id="app">
-    {{ componentName }}
-    <!-- <component :is="componentName"></component> -->
+    <div id="nav">
+      <router-link to="/">Home</router-link> |
+      <router-link to="/about">About</router-link>
+    </div>
+    <router-view/>
   </div>
 </template>
 
-<script>
-// import DragDialog from "./components/drag-dialog";
-// import SlotComponent from "./components/slot-component/index.vue";
-// import eventOn from "@/components/event-on/index.vue"
-export default {
-  name: "App",
-  // components: {
-  //   DragDialog,
-  //   SlotComponent,
-  //   eventOn
-  // },
-  // directives: {
-  //   focus: {
-  //     // 指令的定义
-  //     inserted: function (el) {
-  //       console.log('vm>>>',arguments);
-  //       console.log('el>>>',arguments);
-  //       el.innerHTML='elllllllll';
-  //     },
-  //   },
-  // },
-  // computed: {
-  //   calcNum() {
-  //     return "123";
-  //   },
-  // },
-  data() {
-    return {
-      componentName: '1',
-    };
-  },
-  created(){
-    this.componentName='eventOn'
-  },
-  methods: {
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
 
-  },
-};
-</script>
+#nav {
+  padding: 30px;
+}
+
+#nav a {
+  font-weight: bold;
+  color: #2c3e50;
+}
+
+#nav a.router-link-exact-active {
+  color: #42b983;
+}
+</style>
